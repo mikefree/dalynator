28371ff0cb949d54d79b0dcca804d5dbeaeae90b
mikefree <mike.free@uw.edu>
2011-06-02 12:37:57 -0700

    Edited daly_sim_v1.py via GitHub

--- daly_sim_v1.py
+++ daly_sim_v1.py
@@ -1,48 +1,70 @@
-# Defining functions for calculating YLLs and YLDs
+'''
+Defining functions for calculating YLLs and YLDs
 # Michael Freeman
 # June 2, 2011
+'''
 
 # Define a function for calculating ylls.
-def yll(a, B, c, K, L r):
-# Calculate life expectancy at age a (L) as the standard life expectancy (l) minus the number of years lived (a).
+def yll(a, g, n, B=.04, c=.1658, K=1, r=.03):
+	'''
+	Returns YLL for n individuals gender 'g' who died at age a
+	a = age, no default.
+	g= gender, no default.
+	n = number of deaths, no default.
+	B = .04 (age-weighting)
+	c=.1658 (constant)'
+	K = 1 (age-weighting modulation factor)
+	l = 82.5 (standard life expectancy)
+	r = .03 (discount rate
+	'''
+	# Set the life expectancy based on the gender input:
+	if g=='males':
+		l = 80
+	if g=='females':
+		l=82.5
+	# Calculate life expectancy at age a (L) as the standard life expectancy (l) minus the number of years lived (a).
 	L = l - a
-	value = K*C*e**(r*a)/((r+B)**2)*((e**(-(r+B)*(L+a)))*(-(r+B)*(L+a) - 1) - e**(-(r+B)*a)*(-(r+B)*a - 1)) + ((1-K)/r)*(1-e**(-r*L))
+	value = (K*C*e**(r*a)/((r+B)**2)*((e**(-(r+B)*(L+a)))*(-(r+B)*(L+a) - 1) - e**(-(r+B)*a)*(-(r+B)*a - 1)) + ((1-K)/r)*(1-e**(-r*L)))*n
 	return value
 
-	######## Run through an example, using the standard values ##########
+	''' Run through an example, using the standard values '''
 	
-# Set standard life expectancy, 'l'
-l = 82.5
-
-# Set discount rate, 'r'
-r = .03
-
-# Set the age-weighting modulation factor, 'K'
-K = 1
-
-# Set the parameter from the age-weighting function, 'B'
-B=.04
-
-# Set the constant, 'C'
-C = .1658	
-
-### Choose desired age
+# Choose age
 a = 25
-
-### Test the function:
-yll(a, B, c, K, L r)
+# Choose gender
+g = 'females'
+# Choose number of deaths
+n = 10000
+# Test the function:
+yll(a, g, n)
 	
 # Define a function to calculate ylds.
 
-def yld(a, B, C, D, K, Len):
-	value = D*((K*C*e**(-B*a))/(B**2)*(e**(-B*Len)*(-B*(Len+a)-1) - (-B*a-1)) +((1-K)*Len))
+def yld(a, D, Len, n, B=.04, C=.1658, K=1, r=.03):
+	''' 
+	Returns YLDs for n individuals age 'a' who have a disability with weight 'D' and length 'Len'
+	a = age of onset, no default.
+	D = Disability weight, no default.
+	Len = Length of time with disability, no default.
+	n = number of people with disability, no default.
+	B = .04 (age-weighting)
+	c = .1658 (constant)'
+	K = 1 (age-weighting modulation factor)
+	r = .03 (discount rate
+	'''
+	value = D*((K*C*e**(r*a)/((r+B)**2)*((e**(-(r+B)*(L+a)))*(-(r+B)*(Len+a) - 1) - e**(-(r+B)*a)*(-(r+B)*a - 1)) + ((1-K)/r)*(1-e**(-r*Len))))*n
 	return value
 	
-######## Run through an example, using the standard values, mostly set above ##########
+	''' Run through an example, using the standard values '''
 
-### Choose a disability weight for the example.
+
+# Choose disability weight for the example.
 D = .5
-### Choose a length of disability for the example.
+
+# Choose a length of disability 
 len = 25
-yld(a, B, C, D, K, L)
+
+# Choose number of disabled
+n = 10000
+yld(a, D, len, n)
 	
